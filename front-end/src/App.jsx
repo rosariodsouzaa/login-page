0c9e34e6e5c0f3544e9bf3c3b6dfe5baa1fc8ece
PixelCommit25 <dsouzarosario016@gmail.com>
2026-08-14 16:44:56 +0530

    .

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const API_URL = "https://login-page-phi-sepia.vercel.app";
+  const API_URL = "http://localhost:5000";
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
